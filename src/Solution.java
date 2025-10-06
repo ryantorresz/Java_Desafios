aa2f98cb491c2162fd15aaceb209cad25415cc1e
Ryan Torres <ryantorres77690@gmail.com>
2025-10-06 09:10:04 -0300

    Index of the First Occurrence in a String

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,28 @@
-import java.util.Arrays;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int N = haystack.length();
+        int M = needle.length();
 
-public class Solution {
-    public int removeElement(int[] nums, int val) {
-        int k = 0; // Ponteiro de escrita e contador de elementos válidos
-
-        // Percorre todos os elementos do array
-        for (int i = 0; i < nums.length; i++) {
-
-            // Se o elemento atual for diferente do valor a ser removido (é um elemento VÁLIDO)
-            if (nums[i] != val) {
-                // Move o elemento válido para a posição 'k'
-                nums[k] = nums[i];
-                // Incrementa 'k' para a próxima posição livre
-                k++;
-            }
-            // Se nums[i] == val, 'i' avança, mas 'k' fica parado,
-            // resultando na sobreposição do valor indesejado.
+        if (M == 0) {
+            return 0;
         }
 
-        // 'k' é o novo tamanho lógico do array
-        return k;
+        for (int i = 0; i <= N - M; i++) {
+            int j = 0;
+
+            while (j < M) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                j++;
+            }
+
+            if (j == M) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
 
@@ -27,44 +30,29 @@ class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // --- Exemplo 1 ---
-        int[] nums1 = {3, 2, 2, 3};
-        int val1 = 3;
+        // Exemplo 1: Ocorrência encontrada no início
+        String haystack1 = "sadbutsad";
+        String needle1 = "sad";
+        int index1 = solution.strStr(haystack1, needle1);
+        System.out.println("Input: haystack = \"" + haystack1 + "\", needle = \"" + needle1 + "\"");
+        System.out.println("Output: " + index1); // Esperado: 0
 
-        System.out.println("--- Teste Exemplo 1 ---");
-        System.out.println("Array de entrada: " + Arrays.toString(nums1) + ", Valor a remover: " + val1);
+        System.out.println("---");
 
-        int k1 = solution.removeElement(nums1, val1);
+        // Exemplo 2: Ocorrência não encontrada
+        String haystack2 = "leetcode";
+        String needle2 = "leeto";
+        int index2 = solution.strStr(haystack2, needle2);
+        System.out.println("Input: haystack = \"" + haystack2 + "\", needle = \"" + needle2 + "\"");
+        System.out.println("Output: " + index2); // Esperado: -1
 
-        System.out.println("Saída k: " + k1);
+        System.out.println("---");
 
-        // Imprime os primeiros k elementos (o que interessa ao juiz)
-        System.out.print("Primeiros k elementos de nums: [");
-        for (int i = 0; i < k1; i++) {
-            System.out.print(nums1[i] + (i < k1 - 1 ? ", " : ""));
-        }
-        System.out.println("]");
-        // Resultado esperado: 2, [2, 2]
-
-        System.out.println("\n");
-
-        // --- Exemplo 2 ---
-        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
-        int val2 = 2;
-
-        System.out.println("--- Teste Exemplo 2 ---");
-        System.out.println("Array de entrada: " + Arrays.toString(nums2) + ", Valor a remover: " + val2);
-
-        int k2 = solution.removeElement(nums2, val2);
-
-        System.out.println("Saída k: " + k2);
-
-        // Imprime os primeiros k elementos
-        System.out.print("Primeiros k elementos de nums: [");
-        for (int i = 0; i < k2; i++) {
-            System.out.print(nums2[i] + (i < k2 - 1 ? ", " : ""));
-        }
-        System.out.println("]");
-        // Resultado esperado: 5. A ordem dos elementos é permitida.
+        // Exemplo 3: Ocorrência no meio
+        String haystack3 = "mississippi";
+        String needle3 = "issip";
+        int index3 = solution.strStr(haystack3, needle3);
+        System.out.println("Input: haystack = \"" + haystack3 + "\", needle = \"" + needle3 + "\"");
+        System.out.println("Output: " + index3); // Esperado: 4
     }
 }
