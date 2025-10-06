623cd7fd1531ba39dce6cbd5bc5e40d566645455
Ryan Torres <ryantorres77690@gmail.com>
2025-10-06 14:31:07 -0300

    removeDuplicates

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int k = 1;
 
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
+        for(int i = 1; i < nums.length; i++) {
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+
+                k++;
             }
         }
 
-        return left;
+        return k;
     }
 }
 
@@ -23,28 +22,42 @@ class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] nums = {1, 3, 5, 6};
+        int[] nums1 = {1, 1, 2};
+        int k1 = solution.removeDuplicates(nums1);
 
-        System.out.println("--- Testes de Busca Binária (searchInsert) ---");
+        System.out.println("--- Teste 1: [1, 1, 2] ---");
+        System.out.println("Comprimento (k) retornado: " + k1);
 
-        int target1 = 5;
-        int result1 = solution.searchInsert(nums, target1);
-        System.out.println("Array: [1, 3, 5, 6], Target: " + target1);
-        System.out.println("Resultado: " + result1);
+        System.out.print("Primeiros k elementos de nums: [");
+        for (int i = 0; i < k1; i++) {
+            System.out.print(nums1[i] + (i < k1 - 1 ? ", " : ""));
+        }
+        System.out.println("]");
 
-        int target2 = 2;
-        int result2 = solution.searchInsert(nums, target2);
-        System.out.println("Array: [1, 3, 5, 6], Target: " + target2);
-        System.out.println("Resultado: " + result2 );
 
-        int target3 = 7;
-        int result3 = solution.searchInsert(nums, target3);
-        System.out.println("Array: [1, 3, 5, 6], Target: " + target3);
-        System.out.println("Resultado: " + result3);
+        int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k2 = solution.removeDuplicates(nums2);
 
-        int target4 = 0;
-        int result4 = solution.searchInsert(nums, target4);
-        System.out.println("Array: [1, 3, 5, 6], Target: " + target4);
-        System.out.println("Resultado: " + result4);
+        System.out.println("\n--- Teste 2: [0, 0, 1, 1, 1, 2, 2, 3, 3, 4] ---");
+        System.out.println("Comprimento (k) retornado: " + k2);
+
+        System.out.print("Primeiros k elementos de nums: [");
+        for (int i = 0; i < k2; i++) {
+            System.out.print(nums2[i] + (i < k2 - 1 ? ", " : ""));
+        }
+        System.out.println("]");
+
+
+        int[] nums3 = {5, 6, 7, 8};
+        int k3 = solution.removeDuplicates(nums3);
+
+        System.out.println("\n--- Teste 3: [5, 6, 7, 8] ---");
+        System.out.println("Comprimento (k) retornado: " + k3);
+
+        System.out.print("Primeiros k elementos de nums: [");
+        for (int i = 0; i < k3; i++) {
+            System.out.print(nums3[i] + (i < k3 - 1 ? ", " : ""));
+        }
+        System.out.println("]");
     }
 }
