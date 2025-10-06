82ca64600883d3bbf9dc7cb783b8fbfa08274399
Ryan Torres <ryantorres77690@gmail.com>
2025-10-06 14:54:24 -0300

    lengthOfLastWord

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,12 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        int k = 1;
+    public int lengthOfLastWord(String s) {
+        String trimmedString = s.trim();
 
-        for(int i = 1; i < nums.length; i++) {
 
-            if (nums[i] != nums[k - 1]) {
-                nums[k] = nums[i];
+        int lastSpaceIndex = trimmedString.lastIndexOf(' ');
 
-                k++;
-            }
-        }
 
-        return k;
+        return trimmedString.length() - (lastSpaceIndex + 1);
     }
 }
 
@@ -22,42 +14,26 @@ class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] nums1 = {1, 1, 2};
-        int k1 = solution.removeDuplicates(nums1);
+        System.out.println("--- Testes de Comprimento da Última Palavra ---");
 
-        System.out.println("--- Teste 1: [1, 1, 2] ---");
-        System.out.println("Comprimento (k) retornado: " + k1);
+        String s1 = "Hello World";
+        int result1 = solution.lengthOfLastWord(s1);
+        System.out.println("Input: \"" + s1 + "\"");
+        System.out.println("Output: " + result1 + " \n");
 
-        System.out.print("Primeiros k elementos de nums: [");
-        for (int i = 0; i < k1; i++) {
-            System.out.print(nums1[i] + (i < k1 - 1 ? ", " : ""));
-        }
-        System.out.println("]");
+        String s2 = "   fly me   to   the moon  ";
+        int result2 = solution.lengthOfLastWord(s2);
+        System.out.println("Input: \"" + s2 + "\"");
+        System.out.println("Output: " + result2 + " \n");
 
+        String s3 = "luffy is still joyboy";
+        int result3 = solution.lengthOfLastWord(s3);
+        System.out.println("Input: \"" + s3 + "\"");
+        System.out.println("Output: " + result3 + " \n");
 
-        int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int k2 = solution.removeDuplicates(nums2);
-
-        System.out.println("\n--- Teste 2: [0, 0, 1, 1, 1, 2, 2, 3, 3, 4] ---");
-        System.out.println("Comprimento (k) retornado: " + k2);
-
-        System.out.print("Primeiros k elementos de nums: [");
-        for (int i = 0; i < k2; i++) {
-            System.out.print(nums2[i] + (i < k2 - 1 ? ", " : ""));
-        }
-        System.out.println("]");
-
-
-        int[] nums3 = {5, 6, 7, 8};
-        int k3 = solution.removeDuplicates(nums3);
-
-        System.out.println("\n--- Teste 3: [5, 6, 7, 8] ---");
-        System.out.println("Comprimento (k) retornado: " + k3);
-
-        System.out.print("Primeiros k elementos de nums: [");
-        for (int i = 0; i < k3; i++) {
-            System.out.print(nums3[i] + (i < k3 - 1 ? ", " : ""));
-        }
-        System.out.println("]");
+        String s4 = "   única   ";
+        int result4 = solution.lengthOfLastWord(s4);
+        System.out.println("Input: \"" + s4 + "\"");
+        System.out.println("Output: " + result4 + "\n");
     }
 }
