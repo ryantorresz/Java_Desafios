a28e4a91ab8937fb578b3723d4cbb306a5f397b6
Ryan Torres <ryantorres77690@gmail.com>
2025-10-07 08:36:10 -0300

    climbStairs

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,48 @@
-import java.util.Arrays;
-
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int n = digits.length;
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-
-            digits[i] = 0;
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
         }
 
-        int[] newDigits = new int[n + 1];
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
 
-        newDigits[0] = 1;
+        int anterior_2 = 1;
+        int anterior_1 = 2;
+        int atual = 0;
 
-        return newDigits;
+        for (int i = 3; i <= n; i++) {
+            atual = anterior_1 + anterior_2;
+
+            anterior_2 = anterior_1;
+            anterior_1 = atual;
+        }
+
+        return atual;
     }
-}
 
-class Main {
-
-    public static void printArray(int[] arr) {
-        System.out.print(Arrays.toString(arr));
-    }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution solver = new Solution();
 
-        System.out.println("--- Testes Plus One ---");
+        int n1 = 2;
+        int resultado1 = solver.climbStairs(n1);
+        System.out.println("Para n = " + n1 + " degraus: " + resultado1 + " maneiras.");
 
-        int[] input1 = {1, 2, 3};
-        int[] result1 = solution.plusOne(input1);
-        System.out.print("Input: [1, 2, 3] -> Output: ");
-        printArray(result1);
-        System.out.println();
+        int n2 = 3;
+        int resultado2 = solver.climbStairs(n2);
+        System.out.println("Para n = " + n2 + " degraus: " + resultado2 + " maneiras.");
 
-        int[] input2 = {4, 3, 2, 9};
-        int[] result2 = solution.plusOne(input2);
-        System.out.print("Input: [4, 3, 2, 9] -> Output: ");
-        printArray(result2);
-        System.out.println();
+        int n3 = 5;
+        int resultado3 = solver.climbStairs(n3);
+        System.out.println("Para n = " + n3 + " degraus: " + resultado3 + " maneiras.");
 
-        int[] input3 = {9};
-        int[] result3 = solution.plusOne(input3);
-        System.out.print("Input: [9] -> Output: ");
-        printArray(result3);
-        System.out.println();
-
-        int[] input4 = {9, 9, 9};
-        int[] result4 = solution.plusOne(input4);
-        System.out.print("Input: [9, 9, 9] -> Output: ");
-        printArray(result4);
-        System.out.println();
+        int n4 = 10;
+        int resultado4 = solver.climbStairs(n4);
+        System.out.println("Para n = " + n4 + " degraus: " + resultado4 + " maneiras.");
     }
 }
