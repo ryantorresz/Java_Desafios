78716af9e572270e80381b3eb47ea0a7d5cada11
Ryan Torres <ryantorres77690@gmail.com>
2025-10-07 11:31:33 -0300

    Merge Sorted Array

--- src/Solution.java
+++ src/Solution.java
@@ -1,61 +1,59 @@
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    @Override
-    public String toString() {
-        if (next == null) return String.valueOf(val);
-        return val + " -> " + next.toString();
-    }
-}
+import java.util.Arrays;
 
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        ListNode current = head;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int pMerge = m + n - 1;
 
-        while (current != null && current.next != null) {
+        while (p2 >= 0) {
 
-            if (current.val == current.next.val) {
-                current.next = current.next.next;
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[pMerge] = nums1[p1];
+                p1--;
             } else {
-                current = current.next;
+                nums1[pMerge] = nums2[p2];
+                p2--;
             }
+
+            pMerge--;
         }
-
-        return head;
     }
-
 
     public static void main(String[] args) {
         Solution solver = new Solution();
 
-        ListNode head1 = new ListNode(1, new ListNode(1, new ListNode(2)));
-        System.out.println("Lista Original 1: " + head1);
-        ListNode resultado1 = solver.deleteDuplicates(head1);
-        System.out.println("Resultado 1: " + resultado1);
+        int[] nums1_1 = {1, 2, 3, 0, 0, 0};
+        int m1 = 3;
+        int[] nums2_1 = {2, 5, 6};
+        int n1 = 3;
 
-        System.out.println("---");
+        solver.merge(nums1_1, m1, nums2_1, n1);
+        System.out.println(Arrays.toString(nums1_1));
 
+        int[] nums1_2 = {1};
+        int m2 = 1;
+        int[] nums2_2 = {};
+        int n2 = 0;
 
-        ListNode head2 = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
-        System.out.println("Lista Original 2: " + head2);
-        ListNode resultado2 = solver.deleteDuplicates(head2);
-        System.out.println("Resultado 2: " + resultado2);
+        solver.merge(nums1_2, m2, nums2_2, n2);
+        System.out.println(Arrays.toString(nums1_2));
 
-        System.out.println("---");
+        int[] nums1_3 = {0};
+        int m3 = 0;
+        int[] nums2_3 = {1};
+        int n3 = 1;
 
-        ListNode head3 = new ListNode(1, new ListNode(1, new ListNode(1, new ListNode(1))));
-        System.out.println("Lista Original 3: " + head3);
-        ListNode resultado3 = solver.deleteDuplicates(head3);
-        System.out.println("Resultado 3: " + resultado3);
+        solver.merge(nums1_3, m3, nums2_3, n3);
+        System.out.println(Arrays.toString(nums1_3));
+
+        int[] nums1_4 = {4, 5, 6, 0, 0, 0};
+        int m4 = 3;
+        int[] nums2_4 = {1, 2, 3};
+        int n4 = 3;
+
+        solver.merge(nums1_4, m4, nums2_4, n4);
+        System.out.println(Arrays.toString(nums1_4));
     }
 }
