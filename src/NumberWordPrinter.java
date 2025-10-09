c9f078881839973fd1082c061f9c5d210309fb91
Ryan Torres <ryantorres77690@gmail.com>
2025-10-09 08:32:41 -0300

    NumberWordPrinter refatorado

--- src/NumberWordPrinter.java
+++ src/NumberWordPrinter.java
@@ -3,32 +3,23 @@ import java.util.Scanner;
 public class NumberWordPrinter {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+                System.out.println("-----------------");
+                System.out.println("Digite um número:");
+                System.out.println("-----------------");
+                Scanner scanner = new Scanner(System.in);
+                int n = scanner.nextInt();
+                scanner.close();
 
-        int n = 1;
+                String[] words = {
+                        "", "one", "two", "three", "four",
+                        "five", "six", "seven", "eight", "nine"
+                };
 
-        if (n == 1) {
-            System.out.println("one");
-        } else if (n == 2) {
-            System.out.println("two");
-        } else if (n == 3) {
-            System.out.println("three");
-        } else if (n == 4) {
-            System.out.println("four");
-        } else if (n == 5) {
-            System.out.println("five");
-        } else if (n == 6) {
-            System.out.println("six");
-        } else if (n == 7) {
-            System.out.println("seven");
-        } else if (n == 8) {
-            System.out.println("eight");
-        } else if (n == 9) {
-            System.out.println("nine");
-        } else {
-            System.out.println("Greater than 9");
-        }
+                if (n >= 1 && n <= 9) {
+                    System.out.println(words[n]);
+                } else {
+                    System.out.println("Greater than 9");
+                }
 
-        scanner.close();
     }
 }
