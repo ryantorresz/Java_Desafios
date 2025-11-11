b11ed2f20c41e7c9080c1d89e496cc80266ef0c6
Ryan Torres <ryantorres77690@gmail.com>
2025-11-11 10:52:27 -0300

    Java Arraylist

--- src/Solution.java
+++ src/Solution.java
@@ -1,76 +1,52 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Scanner;
 
 public class Solution {
 
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        if (nums == null || nums.length < 3) {
-            return result;
-        }
-
-        Arrays.sort(nums);
-
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int target = -nums[i];
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-
-                    left++;
-                    right--;
-
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-
-        return result;
-    }
-
     public static void main(String[] args) {
-        int[] nums1 = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> result1 = threeSum(nums1);
-        System.out.println("Input: " + Arrays.toString(nums1));
-        System.out.println("Output: " + result1);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("\n--------------------");
+        // Quantas linhas de números existirão
+        System.out.print("Digite o número de linhas: ");
+        int n = sc.nextInt();
 
-        int[] nums2 = {0, 0, 0};
-        List<List<Integer>> result2 = threeSum(nums2);
-        System.out.println("Input: " + Arrays.toString(nums2));
-        System.out.println("Output: " + result2);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
-        System.out.println("\n--------------------");
+        // Lendo cada linha de números
+        for (int i = 0; i < n; i++) {
+            System.out.print("Quantos números na linha " + (i + 1) + "? ");
+            int d = sc.nextInt();
+            ArrayList<Integer> innerList = new ArrayList<>();
 
-        int[] nums3 = {-2, 0, 1, 1, 2, 2, -1, 0, 5, -3, -3, 3};
-        List<List<Integer>> result3 = threeSum(nums3);
-        System.out.println("Input: " + Arrays.toString(nums3));
-        System.out.println("Output: " + result3);
+            if (d > 0) {
+                System.out.println("Digite " + d + " números separados por espaço:");
+            }
+
+            for (int j = 0; j < d; j++) {
+                innerList.add(sc.nextInt());
+            }
+
+            list.add(innerList);
+        }
+
+        // Quantas consultas (queries)
+        System.out.print("Digite o número de consultas: ");
+        int q = sc.nextInt();
+
+        // Executando as consultas
+        for (int i = 0; i < q; i++) {
+            System.out.print("Consulta " + (i + 1) + " (linha e posição): ");
+            int x = sc.nextInt(); // número da linha (1-indexado)
+            int y = sc.nextInt(); // posição na linha (1-indexado)
+
+            try {
+                int valor = list.get(x - 1).get(y - 1);
+                System.out.println("Resultado: " + valor);
+            } catch (Exception e) {
+                System.out.println("ERROR!");
+            }
+        }
+
+        sc.close();
     }
 }
