a4170099e8b7d55883364b847dc33efae7f49cf4
Ryan Torres <ryantorres77690@gmail.com>
2025-11-20 11:21:38 -0300

    Java BitSet

--- src/Solution.java
+++ src/Solution.java
@@ -1,55 +1,55 @@
+import java.io.*;
 import java.util.*;
-
-class Student {
-    private int id;
-    private String name;
-    private double cgpa;
-
-    public Student(int id, String name, double cgpa) {
-        this.id = id;
-        this.name = name;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public double getCgpa() { return cgpa; }
-}
-
-class StudentComparator implements Comparator<Student> {
-    @Override
-    public int compare(Student s1, Student s2) {
-        // CGPA descending
-        if (s1.getCgpa() != s2.getCgpa()) {
-            return Double.compare(s2.getCgpa(), s1.getCgpa());
-        }
-        // Name ascending
-        int nameCompare = s1.getName().compareTo(s2.getName());
-        if (nameCompare != 0) {
-            return nameCompare;
-        }
-        // ID ascending
-        return Integer.compare(s1.getId(), s2.getId());
-    }
-}
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+
     public static void main(String[] args) {
-        // Criando os inputs diretamente - Sample Input do problema
-        List<Student> students = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        // Adicionando os estudantes do exemplo
-        students.add(new Student(33, "Rumpa", 3.68));
-        students.add(new Student(85, "Ashis", 3.85));
-        students.add(new Student(56, "Samiha", 3.75));
-        students.add(new Student(19, "Samara", 3.75));
-        students.add(new Student(22, "Fahim", 3.76));
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        Collections.sort(students, new StudentComparator());
+        BitSet B1 = new BitSet(N);
+        BitSet B2 = new BitSet(N);
 
-        System.out.println("=== Sample Output ===");
-        for (Student student : students) {
-            System.out.println(student.getName());
+        BitSet[] B = new BitSet[3];
+        B[1] = B1;
+        B[2] = B2;
+
+        for (int i = 0; i < M; i++) {
+            String operation = scanner.next();
+            int arg1 = scanner.nextInt();
+
+            int arg2 = scanner.nextInt();
+
+            switch (operation) {
+                case "AND":
+                    B[arg1].and(B[arg2]);
+                    break;
+
+                case "OR":
+                    B[arg1].or(B[arg2]);
+                    break;
+
+                case "XOR":
+                    B[arg1].xor(B[arg2]);
+                    break;
+
+                case "FLIP":
+                    B[arg1].flip(arg2);
+                    break;
+
+                case "SET":
+                    B[arg1].set(arg2);
+                    break;
+            }
+
+            System.out.println(B1.cardinality() + " " + B2.cardinality());
         }
+
+        scanner.close();
     }
 }
